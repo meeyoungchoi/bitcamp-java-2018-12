8769e42c7198aced87ab085e6bdb9d9267380bbd
meeyoungchoi <goodlife1359@gmail.com>
2019-01-02 12:25:11 +0900

    java-basic프로젝트준비

--- java-basic/src/main/java/Test05.java
+++ java-basic/src/main/java/Test05.java
@@ -1,7 +1,16 @@
 //public class My11 {}
 public class Test05{
 
-    System.out.println("Hello");
+    public static void main(String[] args) {
+    
+    
+        System.out.println("Hello");
+
+
+
+
+}
+   
 
 
 }
