4b1a1d35d6d8eccad09334fa1b0051a7ec170238
meeyoungchoi <goodlife1359@gmail.com>
2019-01-04 13:42:52 +0900

    java-basic프로젝트 준비

---
+++ java-basic/src/main/java/ch02/Test11.java
@@ -0,0 +1,45 @@
+//콘솔로 출력하기
+package ch02;
+
+public class Test11{
+  public static void main(String[] args){
+        
+      //값 출력
+    //println(): 출력한후 줄바꿈한다.
+    System.out.println(20);
+    System.out.println(3.14);
+    System.out.println("Hello");
+    System.out.println('y');
+    System.out.println(true);
+    
+    //값을 주지 않으면 줄바꿈만 수행한다.
+    System.out.println();
+    
+    //print()는 출력만 한다.(줄바꿈 없다.)
+    System.out.print(20);
+    System.out.print(3.14);
+    System.out.print("Hello");
+    System.out.print('y');
+    System.out.print(true);
+    
+    System.out.println('\n');
+    
+    System.out.print("ok\n");//==println("ok")
+    System.out.print("hi\n");//==println("hi")
+    
+    
+        }
+    }
+
+
+
+
+
+
+
+
+
+/**
+
+
+*/
