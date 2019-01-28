60b1b1c673ceb16b067127aea86d607e9c426244
meeyoungchoi <goodlife1359@gmail.com>
2019-01-28 11:07:07 +0900

    java-basic프로젝트준비

--- java-project/src/main/java/com/eomcs/lms/handler/MemberHandler.java
+++ java-project/src/main/java/com/eomcs/lms/handler/MemberHandler.java
@@ -1,29 +1,25 @@
 package com.eomcs.lms.handler;
-
 import java.sql.Date;
 import java.util.Scanner;
-
 import com.eomcs.lms.domain.Member;
-import com.eomcs.lms.util.ArrayList;
+import com.eomcs.util.List;
 
 public class MemberHandler {
- 
+  
   Scanner keyboard;
-  ArrayList<Member> list;
+  List<Member> list;
   
-  
-  public MemberHandler(Scanner keyboard) {
+  public MemberHandler(Scanner keyboard, List<Member> list) {
     this.keyboard = keyboard;
-    this.list = new ArrayList<Member>(20);
+    this.list = list;
   }
   
   public void listMember() {
-	  	Member[] members = list.toArray(new Member[] {});//Member[] members
-    for (Member member : members) {//Member member : members
+    Member[] members = list.toArray(new Member[] {});
+    for (Member member : members) {
       System.out.printf("%3d, %-4s, %-20s, %-15s, %s\n", 
           member.getNo(), member.getName(), 
-          member.getEmail(), member.getTel(),
-          member.getRegisteredDate());
+          member.getEmail(), member.getTel(), member.getRegisteredDate());
     }
   }
 
@@ -50,11 +46,98 @@ public class MemberHandler {
   
     member.setRegisteredDate(new Date(System.currentTimeMillis())); 
     
-    //members[memberIdx] = member;
-    //memberIdx++;
-    list.add(member);//add() 메서드 호출
+    list.add(member);
     
     System.out.println("저장하였습니다.");
   }
 
+  public void detailMember() {
+    System.out.print("번호? ");
+    int no = Integer.parseInt(keyboard.nextLine());
+
+    int index = indexOfMember(no);
+    if (index == -1) {
+      System.out.println("해당 회원을 찾을 수 없습니다.");
+      return;
+    }
+
+    Member member = list.get(index);
+
+    System.out.printf("이름: %s\n", member.getName());
+    System.out.printf("이메일: %s\n", member.getEmail());
+    System.out.printf("암호: %s\n", member.getPassword());
+    System.out.printf("사진: %s\n", member.getPhoto());
+    System.out.printf("전화: %s\n", member.getTel());
+    System.out.printf("가입일: %s\n", member.getRegisteredDate());
+  }
+  
+  public void updateMember() {
+    System.out.print("번호? ");
+    int no = Integer.parseInt(keyboard.nextLine());
+
+    int index = indexOfMember(no);
+    if (index == -1) {
+      System.out.println("해당 회원을 찾을 수 없습니다.");
+      return;
+    }
+    
+    Member member = list.get(index);
+    
+    try {
+      // 기존 값 복제
+      Member temp = member.clone();
+      
+      System.out.printf("이름(%s)? ", member.getName());
+      String input = keyboard.nextLine();
+      if (input.length() > 0) 
+        temp.setName(input);
+      
+      System.out.printf("이메일(%s)? ", member.getEmail());
+      if ((input = keyboard.nextLine()).length() > 0)
+        temp.setEmail(input);
+      
+      System.out.printf("암호(********)? ");
+      if ((input = keyboard.nextLine()).length() > 0)
+        temp.setPassword(input);
+      
+      System.out.printf("사진(%s)? ", member.getPhoto());
+      if ((input = keyboard.nextLine()).length() > 0)
+        temp.setPhoto(input);
+      
+      System.out.printf("전화(%s)? ", member.getTel());
+      if ((input = keyboard.nextLine()).length() > 0)
+        temp.setTel(input);
+      
+      list.set(index, temp);
+      
+      System.out.println("회원을 변경했습니다.");
+      
+    } catch (Exception e) {
+      System.out.println("변경 중 오류 발생!");
+    }
+  }
+  
+  public void deleteMember() {
+    System.out.print("번호? ");
+    int no = Integer.parseInt(keyboard.nextLine());
+
+    int index = indexOfMember(no);
+    if (index == -1) {
+      System.out.println("해당 회원을 찾을 수 없습니다.");
+      return;
+    }
+    
+    list.remove(index);
+    
+    System.out.println("회원을 삭제했습니다.");
+  }
+  
+  private int indexOfMember(int no) {
+    for (int i = 0; i < list.size(); i++) {
+      Member m = list.get(i);
+      if (m.getNo() == no)
+        return i;
+    }
+    return -1;
+  }
 }
